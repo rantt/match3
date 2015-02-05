cee5d9def91681529ad4f485281fb21fb80865a7
Dave Canam <david.canam@gmail.com>
2015-02-05 15:18:40 -0400

    Add download task

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,21 @@
 var gulp = require('gulp');
+var request = require('request');
+var fs = require('fs');
 
 var jshint = require('gulp-jshint');
+
+
+gulp.task('init',['get-phaser', 'get-debug']);
+
+gulp.task('get-phaser', function () {
+  request('https://raw.github.com/photonstorm/phaser/master/build/phaser.min.js').pipe(fs.createWriteStream('src/js/lib/phaser.min.js'));
+  request('https://raw.github.com/photonstorm/phaser/master/build/phaser.map').pipe(fs.createWriteStream('src/js/lib/phaser.map'));
+});
+
+gulp.task('get-debug', function() {
+  request('https://github.com/englercj/phaser-debug/releases/download/v1.1.0/phaser-debug.js').pipe(fs.createWriteStream('src/js/lib/phaser-debug.js'));
+});
+
 
 // gulp.task('copy', function(){
 //   gulp.src('index.js')
